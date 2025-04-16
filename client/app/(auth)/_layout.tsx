d9f9d579e1b8262978ea6eb535e58b7d0c38371d
Humphrey Joshua <joshuahumphrey579@gmail.com>
2025-04-16 16:31:13 +0100

    updated

--- client/app/(auth)/_layout.tsx
+++ client/app/(auth)/_layout.tsx
@@ -1,48 +1,47 @@
-import { Redirect, Stack } from "expo-router";
-import { useAuth } from "@clerk/clerk-expo";
+import { Redirect, Stack } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
 
 export default function AuthRoutesLayout() {
-  const { isLoaded, isSignedIn } = useAuth();
+	const { isLoaded, isSignedIn } = useAuth();
 
-  if (!isLoaded) return null;
+	if (!isLoaded) return null;
 
-  if (isSignedIn) return <Redirect href="/(index)" />;
+	if (isSignedIn) return <Redirect href="/(index)/profile" />;
 
-  return (
-    <Stack
-      screenOptions={{
-        ...(process.env.EXPO_OS !== "ios"
-          ? {}
-          : {
-              headerLargeTitle: true,
-              headerTransparent: true,
-              headerBlurEffect: "systemChromeMaterial",
-              headerLargeTitleShadowVisible: false,
-              headerShadowVisible: true,
-              headerLargeStyle: {
-                // NEW: Make the large title transparent to match the background.
-                backgroundColor: "transparent",
-              },
-            }),
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerTitle: "Welcome back!", headerShown: false }}
-      />
-      <Stack.Screen
-        name="sign-in-email"
-        options={{ headerTitle: "Sign in with Email" }}
-      />
-      <Stack.Screen name="sign-up" options={{ headerTitle: "Sign up" }} />
-      <Stack.Screen
-        name="reset-password"
-        options={{ headerTitle: "Reset password" }}
-      />
-      <Stack.Screen
-        name="privacy-policy"
-        options={{ headerTitle: "Privacy Policy" }}
-      />
-    </Stack>
-  );
+	return (
+		<Stack
+			screenOptions={{
+				...(process.env.EXPO_OS !== 'ios'
+					? {}
+					: {
+							headerLargeTitle: true,
+							headerTransparent: true,
+							headerBlurEffect: 'systemChromeMaterial',
+							headerLargeTitleShadowVisible: false,
+							headerShadowVisible: true,
+							headerLargeStyle: {
+								// NEW: Make the large title transparent to match the background.
+								backgroundColor: 'transparent',
+							},
+					  }),
+			}}>
+			<Stack.Screen
+				name="index"
+				options={{ headerTitle: 'Welcome back!', headerShown: false }}
+			/>
+			<Stack.Screen
+				name="sign-in-email"
+				options={{ headerTitle: 'Sign in with Email' }}
+			/>
+			<Stack.Screen name="sign-up" options={{ headerTitle: 'Sign up' }} />
+			<Stack.Screen
+				name="reset-password"
+				options={{ headerTitle: 'Reset password' }}
+			/>
+			<Stack.Screen
+				name="privacy-policy"
+				options={{ headerTitle: 'Privacy Policy' }}
+			/>
+		</Stack>
+	);
 }
